2674b074e587f49dd0bfc52911d0b76d9f70740f
Sam Luebbert <sgluebbert1@cougars.ccis.edu>
2012-10-04 22:20:32 -0500

    Updates to viewing recipe page

--- objects/recipe.js
+++ objects/recipe.js
@@ -1,9 +1,10 @@
-exports.Recipe = function Recipe(id_, owner_, picture_, name_, dir_)
+exports.Recipe = function Recipe(id_, owner_, picture_, name_, cate_, dir_)
 {
 	this.id = id_;
 	this.owner = owner_;
 	this.picture = picture_;
 	this.name = name_;
+	this.category = cate_;
 	this.directions = dir_;
 	this.ingredients = [];
 
