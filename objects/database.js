325de6bf38b6193ee46b104662d2778cc062ed5c
Sam Luebbert <sgluebbert1@cougars.ccis.edu>
2012-10-08 17:58:42 -0500

    update to add transaction function with additional carried vars support

--- objects/database.js
+++ objects/database.js
@@ -97,6 +97,47 @@ exports.DAO = function()
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Generic transaction function
+	//		this function uses the above query function for everything but the simple BEGIN, ROLLBACK, COMMIT;
+	this.transaction = function(sql_commands, callback, carried_vars)
+	{
+		if (sql_commands.length == 0)
+			callback(true, {});
+
+		this.client.query('BEGIN');
+		
+		// Run first statement
+		this.query(sql_commands[0], output, {sql_commands: sql_commands, i: 0, caller:this, callback: callback, results: [], carried_vars: carried_vars});
+
+		function output(success, result, fields, vars)
+		{
+			if (!success)
+			{
+				// a problem occured! so rollback and return some error details
+				vars.caller.client.query('ROLLBACK');
+				vars.callback(false, {error_line:vars.i}, vars.carried_vars);
+			}
+			else 
+			{
+				// add this result set to the complete set results
+				vars.results.push(result);
+				// Move onto the next sql statement
+				vars.i += 1;
+				if (vars.i < vars.sql_commands.length)
+				{
+					vars.caller.query(vars.sql_commands[vars.i], output, vars);
+				}
+				else // No more statements so finish and return details
+				{
+					vars.caller.client.query('COMMIT');
+					vars.callback(true, {results: vars.results}, vars.carried_vars);
+				}
+			}
+		}
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Call this when you are done with the object
 	this.die = function()
 	{
