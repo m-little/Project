76d56990fbe72377c829f3a6c2b03fd4733414fe
Sam Luebbert <sgluebbert1@cougars.ccis.edu>
2012-12-06 20:42:07 -0600

    changed wiki video to be in content instead of wiki

--- objects/video.js
+++ objects/video.js
@@ -1,7 +1,6 @@
-exports.Video = function Video(id_, name_, caption_, address_)
+exports.Video = function Video(id_, caption_, address_)
 {
 	this.id = id_;
-	this.name = name_;
 	this.caption = caption_;
 	this.address = address_;
 }
